f0d24295d4c921d5f70202a243c7d15ea01e1e66
sharath V <sharathkrml@gmail.com>
2021-11-16 22:06:28 +0530

    added base components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/images/logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <div className="logo">
+        <img src={logo} alt="logo"></img>
+      </div>
+      <div className="calculator-body">
+        <div className="calc-input">
+          <div className="bill">
+            bill
+          </div>
+          <div className="tip">
+            tip
+          </div>
+          <div className="no-of-people">
+            no of people
+          </div>
+        </div>
+        <div className="calc-output">
+          <div className="calc-output-body">
+            <div className="amt-per-person">
+              amount per person
+            </div>
+            <div className="amt-total">
+              amount-total
+            </div>
+            <div className="reset-button">
+              <button class="btn reset-btn" type="button">RESET</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
